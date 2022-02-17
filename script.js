8a968540f4a0f43e82887f2b8608a128cfacb07a
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-02-17 16:01:53 -0300

    req 7 - remove a classe 'selected' de todos itens da div 'color-palette' e a adiciona ao elemento clicado.

--- script.js
+++ script.js
@@ -4,17 +4,14 @@ let pixelMatrix = 5;
 function generatePixels(pixelMatrix) {
   // criar qnt de divs com 'classe="pixel"' (pixelMatrix) vezes
   let pixelBoard = document.getElementById('pixel-board');
-  
+
   // cria linhas (pixelMatrix) vezes
-  for (let i = 1; i <= pixelMatrix; i +=1) {
+  for (let i = 1; i <= pixelMatrix; i += 1) {
     // cria (pixelMatrix) vezes o elemento com 'class="pixel"'
-    for (let j = 1; j <= pixelMatrix; j +=1) {
+    for (let j = 1; j <= pixelMatrix; j += 1) {
       let elementPixel = document.createElement('div');
       elementPixel.className = 'pixel';
-      elementPixel.addEventListener("click",changeColor);
       pixelBoard.appendChild(elementPixel);
-      // referencia para atribuicao de classe ao elemento criado no link:
-      // https://www.horadecodar.com.br/2020/07/12/como-mudar-a-classe-de-um-elemento-com-javascript/
     } // fecha for colunas
     let lineBreak = document.createElement('br');
     pixelBoard.appendChild(lineBreak);
@@ -24,37 +21,19 @@ function generatePixels(pixelMatrix) {
 generatePixels(pixelMatrix);
 
 // seletor de cor
-let colorTargets = document.getElementsByClassName('color');
-for (let i = 0; i<colorTargets.length; i+=1) {
-  colorTargets[i].addEventListener("click", colorSelector);
+// ao clicar numa cor
+let elementSelected = document.querySelector('#color-palette');
+function addClassSelected(event) {
+  let elementoSelectedColor = document.querySelector('.selected');
+    elementoSelectedColor.classList.remove('selected');
+    event.target.classList.add('selected');
 }
-
-let selectedColor = 'black'; // default
-
-function colorSelector (event) {
-  
-  // ao clicar numa cor
-
-  // guarda a cor selecionada
-  let elementReadColor = document.getElementById(''); // nao é por id
-  let newSelectedColor = elementReadColor.style.backgroundColor;
-
-
-  // remove a classe 'selected' da cor onde estava
-  let elementRemoveClass = document.getElementById(selectedColor);
-  elementRemoveClass.classList.toggle('selected');
-  // adiciona classe 'selected' na cor clicada
-  let elementAddClass = document.getElementById(newSelectedColor);
-  elementAddClass.classList.toggle('selected');
-  
-} // fecha function
+elementSelected.addEventListener('click', addClassSelected);
+// .
+// .
+// referencia para atribuicao de classe ao elemento criado no link:
+// https://www.horadecodar.com.br/2020/07/12/como-mudar-a-classe-de-um-elemento-com-javascript/
+// .
+// .
 // ref para o uso do classList.toggle() neste link:
 // https://medium.com/@erycd/como-adicionar-e-remover-classes-de-elementos-html-com-vanilla-js-7b6527b7aa2f
-
-  // altera a cor do 'pixel' clicado
-function changeColor(event) {
-   // elementoClicado.style.background-color = selectedColor;
-}
-
-
-// pausa pra dormir
