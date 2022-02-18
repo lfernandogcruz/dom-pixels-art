0eaaec81c2ef634fc1dd0a4d219271187a403918
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-02-17 23:34:00 -0300

    req 8 - pinta os pixels clicados com a cor selecionada

--- script.js
+++ script.js
@@ -1,34 +1,62 @@
 // criar os 'pixel'screen, iniciando com 5x5
 let pixelMatrix = 5;
-
 function generatePixels(pixelMatrix) {
   // criar qnt de divs com 'classe="pixel"' (pixelMatrix) vezes
   let pixelBoard = document.getElementById('pixel-board');
-
   // cria linhas (pixelMatrix) vezes
   for (let i = 1; i <= pixelMatrix; i += 1) {
-    // cria (pixelMatrix) vezes o elemento com 'class="pixel"'
     for (let j = 1; j <= pixelMatrix; j += 1) {
       let elementPixel = document.createElement('div');
       elementPixel.className = 'pixel';
+      elementPixel.addEventListener('click', changeBgColor);
       pixelBoard.appendChild(elementPixel);
-    } // fecha for colunas
+    }
     let lineBreak = document.createElement('br');
     pixelBoard.appendChild(lineBreak);
-  } // fecha for linhas
-} // fecha function
-
+  }
+}
 generatePixels(pixelMatrix);
 
 // seletor de cor
-// ao clicar numa cor
+// ao clicar numa cor add .selected
 let elementSelected = document.querySelector('#color-palette');
 function addClassSelected(event) {
   let elementoSelectedColor = document.querySelector('.selected');
-    elementoSelectedColor.classList.remove('selected');
-    event.target.classList.add('selected');
+  elementoSelectedColor.classList.remove('selected');
+  event.target.classList.add('selected');
 }
 elementSelected.addEventListener('click', addClassSelected);
+
+
+
+
+
+
+
+
+
+
+// captura bg color de .selected
+
+
+
+
+// altera o bg color do el .pixel
+function changeBgColor(event) {
+  let elementBrushColor = document.querySelector('.selected');
+  let brushColor = elementBrushColor.classList[1];
+  console.log(brushColor);
+  event.target.style.backgroundColor = brushColor;
+}
+
+
+
+
+
+
+
+
+
 // .
 // .
 // referencia para atribuicao de classe ao elemento criado no link:
